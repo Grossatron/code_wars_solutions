f51fcefab14175363303a5d6e2e2e369bb80269d
grossatron <sgross1012@gmail.com>
2024-05-02 07:14:15 -0400

    Updated Find_Maximum_and_Minimum_Values_of_a_List.js Added Fun_with_ES6_Classes_#1_-_People,_people,_people.js to 8_kyu directory Made 7_kyu directory

---
+++ 8_kyu/Find_Maximum_and_Minimum_Values_of_a_List.js
@@ -0,0 +1,42 @@
+'Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.'
+
+
+Solution 1:
+
+let min = function(list){
+    
+    return Math.min(...list)
+}
+
+let max = function(list){
+    
+    return Math.max(...list)
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Solution 2:
+
+let min = function(list){
+    let min = list[0];
+    for(let i = 0; i<list.length; i++){
+        let temp = list[i];
+        if (temp<min){
+            min = temp;
+        }
+    }
+    return min;
+}
+
+let max = function(list){
+    let max = list[0];
+    for(let i = 0; i<list.length; i++){
+        let temp = list[i];
+        if (temp>max){
+            max = temp;
+        }
+    }
+    return max;
+}
+
+
